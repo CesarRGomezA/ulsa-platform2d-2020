7a4313ab4679b415873444f1df90a16e153ca755
CesarRGomezA <CesarRGomezA@gmail.com>
2020-02-04 08:01:26 -0700

    enemigo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+        [SerializeField]
+
+        float moveSpeed = 3.0f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+
+}
